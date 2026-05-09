17b98c0bad9d8c96a21023eb2322672ede9cdef0
Rito Sarkar <ritosarkar79@gmail.com>
2026-05-09 09:07:06 +0530

    renove unused import

--- src/main/java/com/learning/beginner/kafka/wikimedia/controller/WikiMediaController.java
+++ src/main/java/com/learning/beginner/kafka/wikimedia/controller/WikiMediaController.java
@@ -4,7 +4,6 @@ import com.launchdarkly.eventsource.StreamException;
 import com.learning.beginner.kafka.wikimedia.service.WikiMediaProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
